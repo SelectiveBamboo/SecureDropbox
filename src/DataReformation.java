d7ad8fa5cf194cb1fb8b59275e258e27a0f1042d
jules <jda@darold.net>
2020-01-05 12:11:03 +0100

    -Now DataReformation works perfectly
    -Add also comments in DataSplitting and DataReformation

--- src/DataReformation.java
+++ src/DataReformation.java
@@ -1,3 +1,9 @@
+/*
+ * Here we're doing the exact opposite of DataSplitting. Having in parameter an HashMap <Integer, BitSet>
+ * and returning a bitset array with all the bits replaced as they were before DataSplitting.
+ * 
+ */
+
 import java.util.BitSet;
 import java.util.HashMap;
 
@@ -7,13 +13,18 @@ public class DataReformation {
 	
 	public DataReformation(HashMap<Integer, BitSet> hmap) 
 	{
-		int nbArray = hmap.size();
+		int nbLastArray = hmap.size();		//number of the last bitset array in the hashmap (and of bitsets also)
 		
-		int indexBitArray = 0;
+		int indexBitArray = 0;		//index in the returned bitset array
 		
-		for (int i = 0; i < (hmap.get(1).length() -1) ; i++) 
+		for (int i = 0; i < (hmap.get(1).length() -2) ; i++) 
+			/*iterate for bit index in all bitset array in the hashmap
+			 * 
+			 * Until length() -2 cause last bit is not a data to keep (set to 1 if the bit before is relevant)
+			 * and because the before last is handled in another way after this loop since it is not always relevant
+			 */
 		{
-			for (int j = 1; j <= nbArray; j++) 
+			for (int j = 1; j <= nbLastArray; j++) 
 			{
 				if (hmap.get(j).get(i)) 
 				{
@@ -24,26 +35,33 @@ public class DataReformation {
 			}
 		}	
 		
-		int lastIndex = 0;
-		int toHandleArray = nbArray;
+		boolean isIndexRelevant = true;
+		int j = 1;
 		
-		for (int i = hmap.size(); i >= 1; i--) 
+		do		//We iterate until we found the last relevant bit or until we reach the end of the hashmap
 		{
-			if (hmap.get(i).length() > lastIndex)
+			if (hmap.get(j).get(hmap.get(1).length() -2)) 
 			{
-				lastIndex = hmap.get(i).length() -1;
-				toHandleArray = i;
+				reassembledBitArray.set(indexBitArray);
+			}
+			
+			j++;
+			
+			if (j <= nbLastArray) 
+			{
+				if (hmap.get(j).length() != hmap.get(1).length())
+				{
+					isIndexRelevant = false;
+				}
+				
+				indexBitArray++;
+			}
+			else
+			{
+				isIndexRelevant = false;
 			}
 		}
-		
-		
-		for (int i = 1; i <= toHandleArray; i++) 
-		{
-			if (hmap.get(i).get(lastIndex)) 
-			{
-				reassembledBitArray.set(lastIndex + i -1);
-			}
-		}
+		while(isIndexRelevant);
 	}
 
 	
