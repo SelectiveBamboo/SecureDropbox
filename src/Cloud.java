afddc205f0c24a081abffbb32d2ee3c51e485cb6
SelectiveBamboo <58808879+SelectiveBamboo@users.noreply.github.com>
2020-01-19 22:48:20 +0100

    creation of SimpleCloud, extending Clouds
    -Add multiple methods to deal with clouds using simple webdav, using
    sardine

--- src/Cloud.java
+++ src/Cloud.java
@@ -3,34 +3,32 @@ import java.net.InetAddress;
 
 public class Cloud {
 
-	private InetAddress ipAddress;
-	private String url;
-	private String username;
-	private String password;
+	protected InetAddress ipAddress;
+	protected String url;		//Generic URL to reach the cloud, like www.example.com 
+									//with eventually the directory in which write
+									//have to end with an '/'
+	protected String username;
+	protected String password;
 	
-	public Cloud(InetAddress ipAddress) 
-	{
-		this(null, ipAddress, null, null);
-	}
-	
-	public Cloud(String url) 
-	{
-		this(url, null, null, null);
-	}
-	
-	public Cloud(String url, String username, String password) 
-	{
-		this(url, null, username, password);
-	}
-	
-	public Cloud(InetAddress ipAdress, String username, String password) 
-	{
-		this(null, ipAdress, username, password);
-	}
+	/*
+	 * public Cloud(InetAddress ipAddress) { this(null, ipAddress, null, null); }
+	 * 
+	 * public Cloud(String url) { this(url, null, null, null); }
+	 * 
+	 * public Cloud(String url, String username, String password) { this(url, null,
+	 * username, password); }
+	 * 
+	 * public Cloud(InetAddress ipAdress, String username, String password) {
+	 * this(null, ipAdress, username, password); }
+	 */
 	
 	public Cloud(String url, InetAddress ipAddress, String username, String password ) 
 	{
+		
+		//TODO 
+		//Add a test for URL with an exception throw
 		this.url = url;
+		
 		this.ipAddress = ipAddress;
 		this.username = username;
 		this.password = password;
---
+++ src/SimpleCloud.java
@@ -0,0 +1,92 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.util.List;
+
+import com.github.sardine.*;
+
+public class SimpleCloud extends Cloud {
+	
+	public SimpleCloud(InetAddress ipAddress, String username, String password )
+	{
+		super(null, ipAddress, username, password);
+	}
+	
+	public SimpleCloud(String url)
+	{
+		super(url, null, null, null);
+	}
+
+	public SimpleCloud(InetAddress ipAddress)
+	{
+		super(null, ipAddress, null, null);
+	}
+	
+	public SimpleCloud(String url, String username, String password )
+	{
+		super(url, null, username, password);
+	}
+	
+	
+	public void putFile(String nameOnCloud, String localFilePath) throws IOException 
+	{
+		Sardine sardine = SardineFactory.begin(username, password);
+		
+		InputStream	fis = new FileInputStream(new File(localFilePath));
+		sardine.put(url+nameOnCloud, fis);
+		
+	}
+	
+	public void deleteFile(String nameOnCloud) throws IOException 
+	{
+		Sardine sardine = SardineFactory.begin(username, password);		
+		sardine.delete(url+nameOnCloud);
+	}
+	
+	public boolean doesExist(String nameOnCloud)
+	{
+		boolean doesExist = false;
+		
+		Sardine sardine = SardineFactory.begin(username, password);
+		try {
+			doesExist = sardine.exists(url+nameOnCloud);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	
+		return doesExist;
+	}
+
+	public InputStream getFile(String nameOnCloud) throws IOException 
+	{
+		Sardine sardine = SardineFactory.begin(username, password);
+		
+		InputStream is = sardine.get(url+nameOnCloud);
+		
+		return is;
+	}
+	
+	public List<DavResource> list(int depth) throws IOException 
+	{
+		Sardine sardine = SardineFactory.begin(username, password);
+		List<DavResource> resources = sardine.list(url, depth);
+
+		return resources;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
