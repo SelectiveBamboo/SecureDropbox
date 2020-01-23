df3ed1f8f79ba13bddc378a5aadef7984916d434
SelectiveBamboo <58808879+SelectiveBamboo@users.noreply.github.com>
2020-01-23 12:57:27 +0100

    Work on Google Drive, Send file and Get file - Maven Project
    -Switched to Maven Project to deal with dependencies
    -Added methods to connect to drive cloud
    -Added methods to create a file on google drive using byte[],
    java.io.File or an InputStream
    -Added the client_secret credential
    -GetFile in progress, only a method to list files and get their IDs for
    now

---
+++ src/CloudGoogleDrive.java
@@ -0,0 +1,89 @@
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List; 
+import com.google.api.client.http.AbstractInputStreamContent;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+
+public class CloudGoogleDrive {
+	
+    private static File _createGoogleFile(String googleFolderIdParent, String contentType, //
+            String customFileName, AbstractInputStreamContent uploadStreamContent) throws IOException {
+ 
+        File fileMetadata = new File();
+        fileMetadata.setName(customFileName);
+ 
+        List<String> parents = Arrays.asList(googleFolderIdParent);
+        fileMetadata.setParents(parents);
+        //
+        Drive driveService = GoogleDriveUtils.getDriveService();
+ 
+        File file = driveService.files().create(fileMetadata, uploadStreamContent)
+                .setFields("id, webContentLink, webViewLink, parents").execute();
+ 
+        return file;
+    }
+ 
+    // Create Google File from byte[]
+    public static File createGoogleFile(String googleFolderIdParent, String contentType, //
+            String customFileName, byte[] uploadData) throws IOException {
+        //
+        AbstractInputStreamContent uploadStreamContent = new ByteArrayContent(contentType, uploadData);
+        //
+        return _createGoogleFile(googleFolderIdParent, contentType, customFileName, uploadStreamContent);
+    }
+ 
+    // Create Google File from java.io.File
+    public static File createGoogleFile(String googleFolderIdParent, String contentType, //
+            String customFileName, java.io.File uploadFile) throws IOException {
+ 
+        //
+        AbstractInputStreamContent uploadStreamContent = new FileContent(contentType, uploadFile);
+        //
+        return _createGoogleFile(googleFolderIdParent, contentType, customFileName, uploadStreamContent);
+    }
+ 
+    // Create Google File from InputStream
+    public static File createGoogleFile(String googleFolderIdParent, String contentType, //
+            String customFileName, InputStream inputStream) throws IOException {
+ 
+        //
+        AbstractInputStreamContent uploadStreamContent = new InputStreamContent(contentType, inputStream);
+        //
+        return _createGoogleFile(googleFolderIdParent, contentType, customFileName, uploadStreamContent);
+    }
+ 
+    
+    // com.google.api.services.drive.model.File
+    public static final List<File> getGoogleFilesByName(String fileNameLike) throws IOException {
+ 
+        Drive driveService = GoogleDriveUtils.getDriveService();
+ 
+        String pageToken = null;
+        List<File> list = new ArrayList<File>();
+ 
+        String query = " name contains '" + fileNameLike + "' " //
+                + " and mimeType != 'application/vnd.google-apps.folder' ";
+ 
+        do {
+            FileList result = driveService.files().list().setQ(query).setSpaces("drive") //
+                    // Fields will be assigned values: id, name, createdTime, mimeType
+                    .setFields("nextPageToken, files(id, name, createdTime, mimeType)")//
+                    .setPageToken(pageToken).execute();
+            for (File file : result.getFiles()) {
+                list.add(file);
+            }
+            pageToken = result.getNextPageToken();
+        } while (pageToken != null);
+        //
+        return list;
+    }
+
+}
