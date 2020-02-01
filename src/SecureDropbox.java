89e0046080cf3cb82295c6613cae86723218f4bd
SelectiveBamboo <58808879+SelectiveBamboo@users.noreply.github.com>
2020-02-02 00:22:49 +0100

    Start the part in main for inquiring clouds

--- src/SecureDropbox.java
+++ src/SecureDropbox.java
@@ -1,11 +1,12 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class SecureDropbox {
 	
 	private static final String VERSION = "1.0";
 	
-	String path;
-	int cloudsNb = 0;
+	static String path;
+	static int cloudsNb = 0;
 
 	public static void usage()
 	{
@@ -89,7 +90,64 @@ public class SecureDropbox {
 	
 				default:
 					break;
-			}	
+			}
+		}
+		
+		for (int i = 1; i <= cloudsNb; i++) 
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Information about cloud n°" + i);
+			
+			System.out.print("What kind of cloud is it ? ");
+			System.out.println("Type :\n"
+					+ "'1' for Google Drive \n"
+					+ "'2' for Nextcloud/Owncloud");
+			String cloudType = sc.nextLine();
+			
+			switch (cloudType) 
+			{
+				case "1": //Google Drive
+					
+					break;
+
+					
+				case "2": //Nextcloud/Owncloud
+					
+					break;
+			
+					
+				default:
+					System.err.println("ERROR: Can't recognize input.");
+					break;
+			}
+			
+			
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
