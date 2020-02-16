065da9372fe13a2b8a50cc1aac365495415bdd4e
SelectiveBamboo <58808879+SelectiveBamboo@users.noreply.github.com>
2020-02-16 11:49:11 +0100

    Add FileInfos class
    As Object to represent data stored about files' name and path between
    cloud storage and local storage

---
+++ src/FileInfos.java
@@ -0,0 +1,47 @@
+
+public class FileInfos {
+
+	private String pathOnLocal;
+	private String nameOnLocal;
+	private String nameOnCloud;	
+	
+	public FileInfos() 
+	{
+		this.pathOnLocal = null;
+		this.nameOnLocal = null;
+		this.nameOnCloud = null;
+	}
+	
+	public FileInfos(String pathOnLocal, String nameOnLocal, String nameOnCloud) 
+	{
+		this.pathOnLocal = pathOnLocal;
+		this.nameOnLocal = nameOnLocal;
+		this.nameOnCloud = nameOnCloud;
+	}
+	
+	public String getPathOnLocal() 
+	{
+		return pathOnLocal;
+	}
+	public void setPathOnLocal(String pathOnLocal) 
+	{
+		this.pathOnLocal = pathOnLocal;
+	}
+	public String getNameOnLocal() {
+		return nameOnLocal;
+	}
+	public void setNameOnLocal(String nameOnLocal) 
+	{
+		this.nameOnLocal = nameOnLocal;
+	}
+	public String getNameOnCloud() 
+	{
+		return nameOnCloud;
+	}
+	public void setNameOnCloud(String nameOnCloud) 
+	{
+		this.nameOnCloud = nameOnCloud;
+	}
+	
+	
+}
