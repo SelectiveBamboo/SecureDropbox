12427d17881ef56553ccc289fca98bd70a5a6afc
SelectiveBamboo <58808879+SelectiveBamboo@users.noreply.github.com>
2020-02-17 22:28:50 +0100

    Change package

---
+++ src/fr/smb/univ/acy/iut/rt/rt2/project/SecureDropbox/FolderNameNotFoundException.java
@@ -0,0 +1,9 @@
+package fr.smb.univ.acy.iut.rt.rt2.project.SecureDropbox;
+
+public class FolderNameNotFoundException extends Exception {
+	public FolderNameNotFoundException(String folderName, String path)
+	{
+		super("Error the folder named '" + folderName + "' seems not exist in the path '"+path+"'");
+	}
+
+}
